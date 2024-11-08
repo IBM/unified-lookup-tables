f275c64a458b3732e01258b535679633ec33a1d4
Irina Espejo <Irina.Espejo.Morales@ibm.com>
2024-11-08 10:38:20 +0100

    Release fixes (#1)
    * fix: solved multiprocessing error, mismatch in return types and added data collator and pad to tokenizer
    
    Signed-off-by: Irina Espejo Morales <Irina.Espejo.Morales@ibm.com>
    
    * chore: update deps to include accelerate
    
    Signed-off-by: Irina Espejo Morales <Irina.Espejo.Morales@ibm.com>
    
    ---------
    
    Signed-off-by: Irina Espejo Morales <Irina.Espejo.Morales@ibm.com>

--- scripts/training_ult.py
+++ scripts/training_ult.py
@@ -1,13 +1,20 @@
 """Training with ULT for Text."""
 
-from ult.data.text.core import WikiText103Loader
-from ult.configuration import UnicodeTextBWRLETransformParameters
-from ult.transforms import UnicodeTextBWRLETransform
-from loguru import logger
-from datasets import DatasetDict, IterableDatasetDict
 from pathlib import Path
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer
+from datasets import DatasetDict, IterableDatasetDict
+from loguru import logger
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    Trainer,
+    TrainingArguments,
+)
+
+from ult.configuration import UnicodeTextBWRLETransformParameters
+from ult.data.text.core import WikiText103Loader
+from ult.transforms import UnicodeTextBWRLETransform
 
 # Constants, modify as needed
 FIELD_NAME = "text"
@@ -16,54 +23,63 @@ CHECKPOINT = "HuggingFaceTB/SmolLM2-135M"
 OUTPUT_PATH = Path("ult_trainer")
 PATH_TO_LUT = OUTPUT_PATH.joinpath("LUT.json")
 
-# Load Dataset
-dataset = WikiText103Loader()
-logger.info("Data loaded")
 
-# Inizialize ULT transformation to Unicode strings
-unicode_configuration = UnicodeTextBWRLETransformParameters(patch_size=10)
-unicoder = UnicodeTextBWRLETransform(unicode_configuration)
+def main():
+    # Load Dataset
+    dataset = WikiText103Loader().load_splits()
+    logger.info("Data loaded")
 
-# Compute LUT for the training data
-logger.info("Data preprocessing...")
-if isinstance(dataset, DatasetDict):
-    instances = list(dataset["train"][FIELD_NAME])
-    unicoder.add_multiple_instances(instances=instances, num_workers=NUM_WORKERS)
-# NOTE: If the dataset is big list(dataset["train"][FIELD_NAME]) explodes OOM,
-# betther to use lazy IterableDatasetDict
-elif isinstance(dataset, IterableDatasetDict):
-    batched_dataset = dataset["train"].batch(batch_size=32)
-    for sample in iter(batched_dataset):
-        instances = sample[FIELD_NAME]
-        unicoder.add_multiple_instances(instances, num_workers=NUM_WORKERS)
-else:
-    raise ValueError(
-        f"Dataset has to be of type DatasetDict or IterableDatasetDict but received {type(dataset)}."
+    # Inizialize ULT transformation to Unicode strings
+    unicode_configuration = UnicodeTextBWRLETransformParameters(patch_size=10)
+    unicoder = UnicodeTextBWRLETransform(unicode_configuration)
+
+    # Compute LUT for the training data
+    logger.info("Data preprocessing...")
+    if isinstance(dataset, DatasetDict):
+        instances = list(dataset["train"][FIELD_NAME])
+        unicoder.add_multiple_instances(instances=instances, num_workers=NUM_WORKERS)
+    # NOTE: If the dataset is big list(dataset["train"][FIELD_NAME]) explodes OOM,
+    # betther to use lazy IterableDatasetDict
+    elif isinstance(dataset, IterableDatasetDict):
+        batched_dataset = dataset["train"].batch(batch_size=32)
+        for sample in iter(batched_dataset):
+            instances = sample[FIELD_NAME]
+            unicoder.add_multiple_instances(instances, num_workers=NUM_WORKERS)
+    else:
+        raise ValueError(
+            f"Dataset has to be of type DatasetDict or IterableDatasetDict but received {type(dataset)}."
+        )
+    unicoder.save_occurrences(PATH_TO_LUT)
+
+    # Apply preprocessing to the dataset
+    preprocessed_dataset = dataset.map(function=lambda example:{FIELD_NAME: unicoder.encode(example[FIELD_NAME])})
+
+    # Tokenize
+    tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    tokenized_dataset = preprocessed_dataset.map(
+        function=tokenizer, input_columns=FIELD_NAME, fn_kwargs={"truncation": True}
     )
-unicoder.save_occurrences(PATH_TO_LUT)
 
-# Apply preprocessing to the dataset
-preprocessed_dataset = dataset.map(function=unicoder.encode, input_columns=FIELD_NAME)
+    # Model
+    model = AutoModelForCausalLM.from_pretrained(CHECKPOINT)
+    data_collator = DataCollatorForLanguageModeling(mlm=False, tokenizer=tokenizer)
 
-# Tokenize
-tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
-tokenized_dataset = preprocessed_dataset.map(
-    function=tokenizer, input_columns=FIELD_NAME, fn_kwargs={"truncation": True}
-)
+    # Training
+    training_args = TrainingArguments(
+        output_dir=str(OUTPUT_PATH), evaluation_strategy="epoch"
+    )
 
-# Model
-model = AutoModelForCausalLM.from_pretrained(CHECKPOINT)
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["val"],
+        data_collator=data_collator,
+    )
 
-# Training
-training_args = TrainingArguments(
-    output_dir=str(OUTPUT_PATH), evaluation_strategy="epoch"
-)
+    trainer.train()
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset["train"],
-    eval_dataset=tokenized_dataset["val"],
-)
 
-trainer.train()
+if __name__ == '__main__':
+    main()
