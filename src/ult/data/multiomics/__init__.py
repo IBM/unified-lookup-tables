e0d4a168a390eddaf815d9830ce1e0f05d1a44a9
Nikita Janakarajan <nja@zurich.ibm.com>
2025-06-10 14:00:16 +0200

    feat: data loader for omics data
    Signed-off-by: Nikita Janakarajan <nja@zurich.ibm.com>

---
+++ src/ult/data/multiomics/__init__.py
@@ -0,0 +1,8 @@
+"""Multi-omics data loading module."""
+
+__copyright__ = """
+LICENSED INTERNAL CODE. PROPERTY OF IBM.
+IBM Research Licensed Internal Code
+(C) Copyright IBM Corp. 2025
+ALL RIGHTS RESERVED
+"""
