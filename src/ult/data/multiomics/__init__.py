087cee5c9fb6ec1b79128553bdf9e25ee5c52305
Nikita Janakarajan <nja@zurich.ibm.com>
2025-06-11 12:37:23 +0200

    feat: init file
    Signed-off-by: Nikita Janakarajan <nja@zurich.ibm.com>

--- src/ult/data/multiomics/__init__.py
+++ src/ult/data/multiomics/__init__.py
@@ -1,8 +1,1 @@
 """Multi-omics data loading module."""
-
-__copyright__ = """
-LICENSED INTERNAL CODE. PROPERTY OF IBM.
-IBM Research Licensed Internal Code
-(C) Copyright IBM Corp. 2025
-ALL RIGHTS RESERVED
-"""
