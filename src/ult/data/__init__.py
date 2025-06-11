fdb0112fd1955fe06d4cf99dbaf2ebb5ce50d3ab
Nikita Janakarajan <nja@zurich.ibm.com>
2025-06-11 16:37:19 +0200

    feat: added multiomics dataloader
    Signed-off-by: Nikita Janakarajan <nja@zurich.ibm.com>

--- src/ult/data/__init__.py
+++ src/ult/data/__init__.py
@@ -3,9 +3,11 @@
 from .images.cifar import CifarImageDataLoader
 from .images.pass_images import PASSImageDataLoader
 from .text.core import WikiText103Loader
+from .multiomics.tcga_augmented import MultiOmicsDataLoader
 
 DATASETLOADER_REGISTRY = {
     "wikitext": WikiText103Loader,
     "pass": PASSImageDataLoader,
     "cifar10": CifarImageDataLoader,
+    "multiomics": MultiOmicsDataLoader
 }
